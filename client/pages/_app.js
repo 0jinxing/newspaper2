dcaa531c4cf74e37a3d41a65b4e7b19d918bf997
Jinxing Lin <172601673@qq.com>
2019-03-26 18:03:31 +0800

    schedule

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -5,10 +5,8 @@ import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-
-
 const client = new ApolloClient({
-  link: new HttpLink({ uri: '/graphql', fetch }),
+  link: new HttpLink({ uri: '/graphql', fetch, credentials: 'same-origin' }),
   cache: new InMemoryCache(),
 });
 
